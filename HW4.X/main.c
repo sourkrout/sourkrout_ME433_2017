e2fb48450d35391fe108eb560ee5893a8e1f0cc9
sourkrout <kevin@Kevins-MacBook-Pro.local>
2017-04-09 15:57:02 -0500

    started code for hw4

---
+++ HW4.X/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: kevin
+ *
+ * Created on April 9, 2017, 3:33 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+ * 
+ */
+int main(void) {
+
+    return 0;
+}
+
