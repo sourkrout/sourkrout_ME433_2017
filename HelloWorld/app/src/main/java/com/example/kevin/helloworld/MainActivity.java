4e3cb7d5f2ef36ff578aeb0ca423fb21fcd4a1b8
sourkrout <kevin@Kevins-MacBook-Pro.local>
2017-05-09 01:39:06 -0500

    Working Android HW12 Code

---
+++ HelloWorld/app/src/main/java/com/example/kevin/helloworld/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.kevin.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar myControl;
+    TextView myTextView;
+
+    private void setMyControlListener() {
+        myControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                myTextView.setText("We have had this many drinks \uD83E\uDD43 : "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myControl = (SeekBar) findViewById(R.id.seek1);
+
+        myTextView = (TextView) findViewById(R.id.textView01);
+        myTextView.setText("Cheers! \uD83E\uDD42");
+
+        setMyControlListener();
+    }
+}
