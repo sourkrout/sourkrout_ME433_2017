986ba1e60486dd962d70598f3527783244adcc1c
sourkrout <kevin@Kevins-MacBook-Pro.local>
2017-05-16 22:23:24 -0500

    Working HID

---
+++ HW11/firmware/src/app.c
@@ -0,0 +1,505 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+  
+  Company:
+    Microchip Technology Inc.
+  
+  File Name:
+    app.c
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It 
+    implements the logic of the application's state machine and it may call 
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files 
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app.h"
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+int len, i = 0, go=0;
+int startTime = 0;
+
+#define SLAVE_ADDR 0b1101011
+
+void i2c_setup(void);
+void initChip(void);
+void I2C_read_multiple(unsigned char , unsigned char *, int);
+void constructShorts(unsigned char *, int, signed short *);
+
+unsigned char gyroData[14], msg[100];
+signed short gyroShorts[7], temp, gyroX, gyroY, gyroZ, accelX, accelY, accelZ;
+float accelZbuf[10]={0,0,0,0,0,0,0,0,0,0}, firWeights[10]={0.0046,0.0052,-0.0583,0.0502,0.4982,0.4982,0.0502,-0.0583,0.0052,0.0046};
+float firAvg=0,mafAvg=0,iirAvg=0,iirPrev=0,iirA=0.25,iirB=0.75;
+int k,l;
+// *****************************************************************************
+/* Application Data
+  Summary:
+    Holds application data
+  Description:
+    This structure holds the application's data.
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+    
+    Application strings and buffers are be defined outside this structure.
+ */
+
+APP_DATA appData;
+
+/* Mouse Report */
+MOUSE_REPORT mouseReport APP_MAKE_BUFFER_DMA_READY;
+MOUSE_REPORT mouseReportPrevious APP_MAKE_BUFFER_DMA_READY;
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+void APP_USBDeviceHIDEventHandler(USB_DEVICE_HID_INDEX hidInstance,
+        USB_DEVICE_HID_EVENT event, void * eventData, uintptr_t userData) {
+    APP_DATA * appData = (APP_DATA *) userData;
+
+    switch (event) {
+        case USB_DEVICE_HID_EVENT_REPORT_SENT:
+
+            /* This means the mouse report was sent.
+             We are free to send another report */
+
+            appData->isMouseReportSendBusy = false;
+            break;
+
+        case USB_DEVICE_HID_EVENT_REPORT_RECEIVED:
+
+            /* Dont care for other event in this demo */
+            break;
+
+        case USB_DEVICE_HID_EVENT_SET_IDLE:
+
+            /* Acknowledge the Control Write Transfer */
+            USB_DEVICE_ControlStatus(appData->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+
+            /* save Idle rate received from Host */
+            appData->idleRate = ((USB_DEVICE_HID_EVENT_DATA_SET_IDLE*) eventData)->duration;
+            break;
+
+        case USB_DEVICE_HID_EVENT_GET_IDLE:
+
+            /* Host is requesting for Idle rate. Now send the Idle rate */
+            USB_DEVICE_ControlSend(appData->deviceHandle, &(appData->idleRate), 1);
+
+            /* On successfully receiving Idle rate, the Host would acknowledge back with a
+               Zero Length packet. The HID function driver returns an event
+               USB_DEVICE_HID_EVENT_CONTROL_TRANSFER_DATA_SENT to the application upon
+               receiving this Zero Length packet from Host.
+               USB_DEVICE_HID_EVENT_CONTROL_TRANSFER_DATA_SENT event indicates this control transfer
+               event is complete */
+
+            break;
+
+        case USB_DEVICE_HID_EVENT_SET_PROTOCOL:
+            /* Host is trying set protocol. Now receive the protocol and save */
+            appData->activeProtocol = *(USB_HID_PROTOCOL_CODE *) eventData;
+
+            /* Acknowledge the Control Write Transfer */
+            USB_DEVICE_ControlStatus(appData->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+            break;
+
+        case USB_DEVICE_HID_EVENT_GET_PROTOCOL:
+
+            /* Host is requesting for Current Protocol. Now send the Idle rate */
+            USB_DEVICE_ControlSend(appData->deviceHandle, &(appData->activeProtocol), 1);
+
+            /* On successfully receiving Idle rate, the Host would acknowledge
+              back with a Zero Length packet. The HID function driver returns
+              an event USB_DEVICE_HID_EVENT_CONTROL_TRANSFER_DATA_SENT to the
+              application upon receiving this Zero Length packet from Host.
+              USB_DEVICE_HID_EVENT_CONTROL_TRANSFER_DATA_SENT event indicates
+              this control transfer event is complete */
+            break;
+
+        case USB_DEVICE_HID_EVENT_CONTROL_TRANSFER_DATA_SENT:
+            break;
+
+        default:
+            break;
+    }
+}
+
+/*******************************************************************************
+  Function:
+    void APP_USBDeviceEventHandler (USB_DEVICE_EVENT event,
+        USB_DEVICE_EVENT_DATA * eventData)
+  Summary:
+    Event callback generated by USB device layer.
+  Description:
+    This event handler will handle all device layer events.
+  Parameters:
+    None.
+  Returns:
+    None.
+ */
+
+void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr_t context) {
+    USB_DEVICE_EVENT_DATA_CONFIGURED * configurationValue;
+    switch (event) {
+        case USB_DEVICE_EVENT_SOF:
+            /* This event is used for switch debounce. This flag is reset
+             * by the switch process routine. */
+            appData.sofEventHasOccurred = true;
+            appData.setIdleTimer++;
+            break;
+        case USB_DEVICE_EVENT_RESET:
+        case USB_DEVICE_EVENT_DECONFIGURED:
+
+            /* Device got deconfigured */
+
+            appData.isConfigured = false;
+            appData.isMouseReportSendBusy = false;
+            appData.state = APP_STATE_WAIT_FOR_CONFIGURATION;
+            //appData.emulateMouse = true;
+            //BSP_LEDOn ( APP_USB_LED_1 );
+            //BSP_LEDOn ( APP_USB_LED_2 );
+            //BSP_LEDOff ( APP_USB_LED_3 );
+
+            break;
+
+        case USB_DEVICE_EVENT_CONFIGURED:
+
+            /* Device is configured */
+            configurationValue = (USB_DEVICE_EVENT_DATA_CONFIGURED *) eventData;
+            if (configurationValue->configurationValue == 1) {
+                appData.isConfigured = true;
+
+                //BSP_LEDOff ( APP_USB_LED_1 );
+                //BSP_LEDOff ( APP_USB_LED_2 );
+                //BSP_LEDOn ( APP_USB_LED_3 );
+
+                /* Register the Application HID Event Handler. */
+
+                USB_DEVICE_HID_EventHandlerSet(appData.hidInstance,
+                        APP_USBDeviceHIDEventHandler, (uintptr_t) & appData);
+            }
+            break;
+
+        case USB_DEVICE_EVENT_POWER_DETECTED:
+
+            /* VBUS was detected. We can attach the device */
+            USB_DEVICE_Attach(appData.deviceHandle);
+            break;
+
+        case USB_DEVICE_EVENT_POWER_REMOVED:
+
+            /* VBUS is not available any more. Detach the device. */
+            USB_DEVICE_Detach(appData.deviceHandle);
+            break;
+
+        case USB_DEVICE_EVENT_SUSPENDED:
+            //BSP_LEDOff ( APP_USB_LED_1 );
+            //BSP_LEDOn ( APP_USB_LED_2 );
+            //BSP_LEDOn ( APP_USB_LED_3 );
+            break;
+
+        case USB_DEVICE_EVENT_RESUMED:
+        case USB_DEVICE_EVENT_ERROR:
+        default:
+            break;
+
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Initialize(void) {
+    /* Place the App state machine in its initial state. */
+    appData.state = APP_STATE_INIT;
+    appData.deviceHandle = USB_DEVICE_HANDLE_INVALID;
+    appData.isConfigured = false;
+    //appData.emulateMouse = true;
+    appData.hidInstance = 0;
+    appData.isMouseReportSendBusy = false;
+    
+    i2c_setup();
+    initChip();    
+    
+    startTime = _CP0_GET_COUNT();
+}
+
+/******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Tasks(void) {
+    static int8_t vector = 0;
+    static uint8_t movement_length = 0;
+    int8_t dir_table[] = {-4, -4, -4, 0, 4, 4, 4, 0};
+    static uint8_t inc = 0, mouseX=0, mouseY=0;
+    static signed short gyroBufX[10],gyroBufY[10],mafX=0,mafY=0;
+    int i;
+
+    /* Check the application's current state. */
+    switch (appData.state) {
+            /* Application's initial state. */
+        case APP_STATE_INIT:
+        {
+            /* Open the device layer */
+            appData.deviceHandle = USB_DEVICE_Open(USB_DEVICE_INDEX_0,
+                    DRV_IO_INTENT_READWRITE);
+
+            if (appData.deviceHandle != USB_DEVICE_HANDLE_INVALID) {
+                /* Register a callback with device layer to get event notification (for end point 0) */
+                USB_DEVICE_EventHandlerSet(appData.deviceHandle,
+                        APP_USBDeviceEventHandler, 0);
+
+                appData.state = APP_STATE_WAIT_FOR_CONFIGURATION;
+            } else {
+                /* The Device Layer is not ready to be opened. We should try
+                 * again later. */
+            }
+            break;
+        }
+
+        case APP_STATE_WAIT_FOR_CONFIGURATION:
+
+            /* Check if the device is configured. The 
+             * isConfigured flag is updated in the
+             * Device Event Handler */
+
+            if (appData.isConfigured) {
+                appData.state = APP_STATE_MOUSE_EMULATE;
+            }
+            break;
+
+        case APP_STATE_MOUSE_EMULATE:
+            
+            // every 50th loop, or 20 times per second
+            if (movement_length > 50) {
+                if (inc==9){
+                I2C_read_multiple(0x20,gyroData,14);
+                constructShorts(gyroData,14,gyroShorts);
+                gyroX=gyroShorts[1];
+                gyroY=gyroShorts[2];
+                gyroBufX[inc]=gyroX;
+                gyroBufY[inc]=gyroY;
+                
+                for (i=0;i<10;i++){
+                   mafX+=gyroBufX[i];
+                   mafY+=gyroBufY[i];
+                }
+                
+                if (gyroX>=2000){
+                    mouseX=2;
+                } else if (gyroX<=-2000){
+                    mouseX=-2;
+                } else {
+                    mouseX=0;
+                }
+                
+                if (gyroY>=2000){
+                    mouseY=2;
+                } else if (gyroY<=-2000){
+                    mouseY=-2;
+                } else {
+                    mouseY=0;
+                }
+               
+                
+
+                appData.mouseButton[0] = MOUSE_BUTTON_STATE_RELEASED;
+                appData.mouseButton[1] = MOUSE_BUTTON_STATE_RELEASED;
+                appData.xCoordinate = (int8_t) mouseX;//mafX/10000;
+                appData.yCoordinate = (int8_t) mouseY;//mafY/10000;
+                movement_length = 0;
+                inc=0;
+                mafX=0;
+                mafY=0;
+                } else {
+                I2C_read_multiple(0x20,gyroData,14);
+                constructShorts(gyroData,14,gyroShorts);
+                gyroX=gyroShorts[1];
+                gyroY=gyroShorts[2];
+                gyroBufX[inc]=gyroX;
+                gyroBufY[inc]=gyroY;
+                appData.xCoordinate = (int8_t) 0;
+                appData.yCoordinate = (int8_t) 0;
+                inc++;
+                }
+               
+                
+            }
+
+            if (!appData.isMouseReportSendBusy) {
+                /* This means we can send the mouse report. The
+                   isMouseReportBusy flag is updated in the HID Event Handler. */
+
+                appData.isMouseReportSendBusy = true;
+
+                /* Create the mouse report */
+
+                MOUSE_ReportCreate(appData.xCoordinate, appData.yCoordinate,
+                        appData.mouseButton, &mouseReport);
+
+                if (memcmp((const void *) &mouseReportPrevious, (const void *) &mouseReport,
+                        (size_t)sizeof (mouseReport)) == 0) {
+                    /* Reports are same as previous report. However mouse reports
+                     * can be same as previous report as the coordinate positions are relative.
+                     * In that case it needs to be sent */
+                    if ((appData.xCoordinate == 0) && (appData.yCoordinate == 0)) {
+                        /* If the coordinate positions are 0, that means there
+                         * is no relative change */
+                        if (appData.idleRate == 0) {
+                            appData.isMouseReportSendBusy = false;
+                        } else {
+                            /* Check the idle rate here. If idle rate time elapsed
+                             * then the data will be sent. Idle rate resolution is
+                             * 4 msec as per HID specification; possible range is
+                             * between 4msec >= idlerate <= 1020 msec.
+                             */
+                            if (appData.setIdleTimer
+                                    >= appData.idleRate * 4) {
+                                /* Send REPORT as idle time has elapsed */
+                                appData.isMouseReportSendBusy = true;
+                            } else {
+                                /* Do not send REPORT as idle time has not elapsed */
+                                appData.isMouseReportSendBusy = false;
+                            }
+                        }
+                    }
+
+                }
+                if (appData.isMouseReportSendBusy == true) {
+                    /* Copy the report sent to previous */
+                    memcpy((void *) &mouseReportPrevious, (const void *) &mouseReport,
+                            (size_t)sizeof (mouseReport));
+
+                    /* Send the mouse report. */
+                    USB_DEVICE_HID_ReportSend(appData.hidInstance,
+                            &appData.reportTransferHandle, (uint8_t*) & mouseReport,
+                            sizeof (MOUSE_REPORT));
+                    appData.setIdleTimer = 0;
+                }
+                movement_length++;
+            }
+
+            break;
+
+        case APP_STATE_ERROR:
+
+            break;
+
+            /* The default state should never be executed. */
+        default:
+        {
+            /* TODO: Handle error in application's state machine. */
+            break;
+        }
+    }
+}
+
+void i2c_setup(void) {
+  i2c_master_setup();
+  ANSELBbits.ANSB2 = 0;
+  ANSELBbits.ANSB3 = 0;
+}
+
+void initChip(void){
+    i2c_master_start();
+    i2c_master_send(SLAVE_ADDR << 1);
+    i2c_master_send(0x10);
+    i2c_master_send(0x82); 
+    i2c_master_stop();
+    
+    i2c_master_start();
+    i2c_master_send(SLAVE_ADDR << 1);
+    i2c_master_send(0x11);
+    i2c_master_send(0x88); 
+    i2c_master_stop();
+}
+
+void I2C_read_multiple(unsigned char reg, unsigned char * data, int length){
+    int i;
+    
+    i2c_master_start();
+    i2c_master_send(SLAVE_ADDR << 1);
+    i2c_master_send(reg);
+    i2c_master_restart();
+    i2c_master_send((SLAVE_ADDR << 1) | 1);
+    
+    for (i=0;i<(length-1);i++){
+        *(data+i)=i2c_master_recv();
+        i2c_master_ack(0);
+    }
+    *(data+(length-1))=i2c_master_recv();
+    i2c_master_ack(1);
+    i2c_master_stop();   
+}
+
+void constructShorts(unsigned char * data, int length, signed short * shorts){
+    int i, maxi;
+    maxi=length/2;
+    
+    for (i=0;i<maxi;i++){
+        signed short store1;
+        store1=data[((2*i)+1)]<<8;
+        store1=store1 | (data[(2*i)]);
+        shorts[i]=store1;
+    }
+}
+
+/*******************************************************************************
+ End of File
+ */
